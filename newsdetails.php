b08b8ac3322cc0ed288d201b2be05a85f9b60a77
OmarComish <omarthommy@gmail.com>
2019-05-16 09:57:23 +0200

    added the newsdetail page

---
+++ newsdetails.php
@@ -0,0 +1,61 @@
+<?php include_once('page_sections/header.php') ?>
+
+<!-- Banner -->
+
+<div class="home">
+    <div class="background_image_resources" style="background-image: url(images/resources.jpg);"></div>
+    <div class="overlay"></div>
+    <div class="home_container">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="home_content">
+                        <div class="home_title">News details</div>
+                        <!-- <div class="home_subtitle">Pilates, Yoga, Fitness, Spinning & many more</div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Gallery -->
+<div class="about">
+    <div class="container about_container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="about_content">
+                    <div class="section_title_container">
+                       <div class="section_subtitle">welcome to BaoBab Health</div>
+                       <br>
+                           <div class="col-md-12">
+                                <div class="blog-img">
+                                    <img src="images/news_blog3_img.jpg" alt="">
+                                </div>
+                                 <br>
+                                <div class="blog-title">
+                                    <h4><a href="#">BHT Rolls out Electronic Stock Management System</a></h4>
+                                      <div class="news-meta">
+                                        <ul>
+                                            <li>07 February 2019</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <br>
+                                <p>
+                                   Stock management remains one of the fundamental assignments for any successful organization.
+                                   Such is the case because every organisation would want to see the purchased equipment or goods being put to proper
+                                   use and generate the desired returns for the growth of the organisation. However most organisations find this task to be “alot of work"
+                                   <i class="fa fa-dot-circle-o" aria-hidden="true"></i>
+
+                                </p>
+                           </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="bottom_page_spacer"></div>
+
+<?php include_once('page_sections/footer.php') ?>
